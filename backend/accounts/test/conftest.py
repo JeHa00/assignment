b8d1ea4a3d54888bf8c0ddaeb6f0c192f5cd27e2
jeha00 <rudtls0611@naver.com>
2023-11-17 18:24:59 +0900

    TEST: add fake_another_user and fake_authorization_header fixtures

--- backend/accounts/test/conftest.py
+++ backend/accounts/test/conftest.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from rest_framework.test import APIClient
+from django.urls import reverse
 import pytest
 
 from common.utils import random_lower_string
@@ -19,6 +21,44 @@ def fake_user() -> dict:
         "username": f"user-{random_lower_string(k=10)}",
         "password": random_lower_string(k=10),
         "team": User.TeamChoices.DANBIE,
+    }
+
+    serializer = SignupSerializer(data_to_be_created)
+
+    new_user = User.objects.create_user(**serializer.data)
+
+    data_to_be_created.pop("team")
+
+    return {"user_object": new_user, "login_data": deepcopy(data_to_be_created)}
+
+
+@pytest.fixture(scope="function")
+@pytest.mark.django_db
+def fake_authorization_header(
+    client: APIClient(),
+    fake_user: dict,
+) -> dict:
+    login_data = fake_user["login_data"]
+
+    response = client.post(reverse("login"), login_data)
+
+    access_token = response.data["token_information"]["access_token"]
+
+    return {"Authorization": f"Bearer {access_token}"}
+
+
+@pytest.fixture(scope="function")
+@pytest.mark.django_db
+def fake_another_user() -> dict:
+    """fake_user와 그룹이 다른 테스트용 유저를 1명 생성한다.
+
+    Returns:
+        dict: 새로 생성된 User 객체 와 로그인 정보를 반환
+    """
+    data_to_be_created = {
+        "username": f"user-{random_lower_string(k=10)}",
+        "password": random_lower_string(k=10),
+        "team": User.TeamChoices.CHEOLLO,
     }
 
     serializer = SignupSerializer(data_to_be_created)
