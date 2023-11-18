b69f5bf860ce2aa88ba990a3de8fd418430e529d
jeha00 <rudtls0611@naver.com>
2023-11-18 18:55:58 +0900

    FEAT: add urls about subtasks

--- backend/subtasks/urls.py
+++ backend/subtasks/urls.py
@@ -1,1 +1,19 @@
+from django.urls import path
 
+from subtasks import views
+
+
+urlpatterns = [
+    path("/subtasks", views.SubTaskListView.as_view(), name="subtask_list"),
+    path(
+        "/tasks/<int:task_id>/subtasks",
+        views.SubtaskCreateView.as_view(),
+        name="subtask_create",
+    ),
+    path("/subtasks/<int:pk>", views.SubtaskView.as_view(), name="subtask"),
+    path(
+        "/subtasks/<int:pk>/completion",
+        views.MarkAsCompletionView.as_view(),
+        name="subtask_completion",
+    ),
+]
