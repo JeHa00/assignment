b2849f09a5c57dbe61b8149d1bd5bc2d4eb9fd38
jeha00 <rudtls0611@naver.com>
2023-11-19 08:03:08 +0900

    update: update SubtaskSerializer

--- backend/subtasks/serializers.py
+++ backend/subtasks/serializers.py
@@ -1,17 +1,13 @@
 from rest_framework import serializers
 
-from tasks.serializers import TaskSerializer
 from subtasks.models import SubTask
 
 
 class SubtaskSerializer(serializers.ModelSerializer):
-    task = TaskSerializer(read_only=True)
-
     class Meta:
         model = SubTask
         fields = [
             "id",
-            "task",
             "team",
             "is_completed",
             "completed_at",
