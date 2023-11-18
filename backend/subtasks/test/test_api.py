47a9cf0d761d7f1e8f381c02ce18c9bfd0cd7b4e
jeha00 <rudtls0611@naver.com>
2023-11-18 16:55:01 +0900

    TEST: add tests about mark_as_completion

--- backend/subtasks/test/test_api.py
+++ backend/subtasks/test/test_api.py
@@ -82,6 +82,65 @@ def test_get_subtasks_if_success(
 
 
 @pytest.mark.django_db
+@pytest.mark.mark_as_completion
+def test_mark_as_completion_if_success(
+    client: APIClient(),
+    fake_authorization_header: dict,
+    fake_subtask: Task,
+):
+    url = reverse("subtask_completion", args=[fake_subtask.id])
+
+    assert fake_subtask.is_completed is False
+
+    response = client.patch(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["message"] == "success to mark as completion"
+
+    refreshed_task = SubTask.objects.filter(id=fake_subtask.id).last()
+
+    assert refreshed_task.is_completed is True
+
+
+@pytest.mark.django_db
+@pytest.mark.mark_as_completion
+def test_mark_as_completion_if_not_exist_task(
+    client: APIClient(),
+    fake_authorization_header: dict,
+):
+    not_exist_subtask_id = 1
+    url = reverse("subtask_completion", args=[not_exist_subtask_id])
+
+    response = client.patch(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.data["detail"] == "해당되는 하위 업무를 찾을 수 없습니다."
+    assert response.data["detail"].code == "SUBTASK_NOT_FOUND"
+
+
+@pytest.mark.django_db
+@pytest.mark.mark_as_completion
+def test_mark_as_completion_if_forbidden(
+    client: APIClient(),
+    fake_authorization_header: dict,
+    fake_another_subtask: Task,
+):
+    url = reverse("subtask_completion", args=[fake_another_subtask.id])
+
+    assert fake_another_subtask.is_completed is False
+
+    response = client.patch(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert (
+        response.data["detail"] == "You do not have permission to perform this action."
+    )
+    assert response.data["detail"].code == "permission_denied"
+
+    assert fake_another_subtask.is_completed is False
+
+
+@pytest.mark.django_db
 @pytest.mark.delete_a_subtask
 def test_delete_subtask_if_success(
     client: APIClient(),
