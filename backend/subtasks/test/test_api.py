ef6ef3a1d712696d6e0a0867ffb37565cba0c7f6
jeha00 <rudtls0611@naver.com>
2023-11-18 16:48:56 +0900

    TEST: add tests about delete a subtask

--- backend/subtasks/test/test_api.py
+++ backend/subtasks/test/test_api.py
@@ -79,3 +79,55 @@ def test_get_subtasks_if_success(
 
         assert "results" in response.data
         assert len(response.data["results"]) == 10
+
+
+@pytest.mark.django_db
+@pytest.mark.delete_a_subtask
+def test_delete_subtask_if_success(
+    client: APIClient(),
+    fake_authorization_header: dict,
+    fake_subtask: Task,
+):
+    url = reverse("subtask", args=[fake_subtask.id])
+
+    response = client.delete(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    assert SubTask.objects.filter(id=fake_subtask.id).exists() is False
+
+
+@pytest.mark.django_db
+@pytest.mark.delete_a_subtask
+def test_delete_subtask_if_not_exist_task(
+    client: APIClient(),
+    fake_authorization_header: dict,
+):
+    not_exist_subtask_id = 1
+    url = reverse("subtask", args=[not_exist_subtask_id])
+
+    response = client.delete(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.data["detail"] == "해당되는 하위 업무를 찾을 수 없습니다."
+    assert response.data["detail"].code == "SUBTASK_NOT_FOUND"
+
+
+@pytest.mark.django_db
+@pytest.mark.delete_a_subtask
+def test_delete_subtask_if_forbidden(
+    client: APIClient(),
+    fake_authorization_header: dict,
+    fake_another_subtask: Task,
+):
+    url = reverse("subtask", args=[fake_another_subtask.id])
+
+    response = client.delete(url, headers=fake_authorization_header)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert (
+        response.data["detail"] == "You do not have permission to perform this action."
+    )
+    assert response.data["detail"].code == "permission_denied"
+
+    assert SubTask.objects.filter(id=fake_another_subtask.id).exists() is True
