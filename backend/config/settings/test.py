cb5c189cecc9793570cb1f3adbd64868b998885d
jeha00 <rudtls0611@naver.com>
2023-11-21 20:55:33 +0900

    CONF: change db sqlite3 to Mysql

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -13,9 +13,15 @@ MIDDLEWARE = [
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": env.str("TEST_DB_NAME"),
+        "USER": env.str("TEST_DB_USER"),
+        "PASSWORD": env.str("TEST_DB_PASSWORD"),
+        "HOST": "localhost",
+        "PORT": env.str("TEST_DB_PORT"),
+        "TEST": {"NAME": "test"},
     },
 }
